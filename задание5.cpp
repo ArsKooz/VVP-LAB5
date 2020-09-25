ab30ac7c03f975d9c41d7d6cd5981447440e1ea6
ArsKooz <71432162+ArsKooz@users.noreply.github.com>
2020-09-25 18:50:48 +0300

    Update задание5.cpp

--- задание5.cpp
+++ задание5.cpp
@@ -6,7 +6,7 @@ int main()
     setlocale(LC_ALL, "Rus");
     int x1, x2, x3, y1, y2, y3;
     double resAC, resBC,resAB,P,S;
-    cout << "ââåäèòå êîîðäèíàòû òî÷åê A, B, C\n";
+    cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð·Ð½Ð°Ñ‡ÐµÐ½Ð¸Ðµ ÐºÐ¾Ð¾Ñ€Ð´Ð¸Ð½Ð°Ñ‚ A, B, C\n";
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     resAC = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
     resBC = sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
